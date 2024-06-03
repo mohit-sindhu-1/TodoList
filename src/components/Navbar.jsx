09eea96a941c5200487199b431bc25b8fa334e3b
Mohit Sindhu <mohitsindhu@gmail.com>
2024-06-03 17:45:08 +0530

    message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,6 @@ const Navbar = () => {
             <div className="logo">
                 <span className='font-bold text-xl mx-9'>iTask</span>
             </div>
-            {/* <ul className="flex gap-7 mx-9">
-                <li className='cursor-pointer hover:font-bold w-11'>Home</li>
-                <li className='cursor-pointer hover:font-bold w-[88px]'>Your Tasks</li>
-            </ul> */}
         </nav>
     )
 }
